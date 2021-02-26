f53b5911fcfa15f7f9737ab3793dc2434a832168
JVViacrusis <45835322+JVViacrusis@users.noreply.github.com>
2021-02-25 17:16:05 -0800

    Fresh Start
    Deleted all non-github files and replaced it with a new WPILib project

--- 2021/src/main/java/frc/robot/RobotContainer.java
+++ 2021/src/main/java/frc/robot/RobotContainer.java
@@ -5,22 +5,10 @@
 package frc.robot;
 
 import edu.wpi.first.wpilibj.GenericHID;
-import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj.XboxController;
-import frc.robot.commands.ArcadeDrive;
 import frc.robot.commands.ExampleCommand;
-import frc.robot.commands.LEDwhite;
-import frc.robot.commands.STOP;
-import frc.robot.commands.Shoot;
-import frc.robot.commands.ShootwithLED;
-import frc.robot.subsystems.Agitator;
-import frc.robot.subsystems.DriveTrain;
 import frc.robot.subsystems.ExampleSubsystem;
-import frc.robot.subsystems.Feeder;
-import frc.robot.subsystems.LED;
-import frc.robot.subsystems.Launcher;
 import edu.wpi.first.wpilibj2.command.Command;
-import edu.wpi.first.wpilibj2.command.button.JoystickButton;
 
 /**
  * This class is where the bulk of the robot should be declared. Since Command-based is a
@@ -31,19 +19,9 @@ import edu.wpi.first.wpilibj2.command.button.JoystickButton;
 public class RobotContainer {
   // The robot's subsystems and commands are defined here...
   private final ExampleSubsystem m_exampleSubsystem = new ExampleSubsystem();
-  public static final Launcher LAUNCHER = new Launcher();
-  public static final Feeder FEEDER = new Feeder();
-  public static final Agitator m_ag = new Agitator();
-  public static final LED le = new LED();
-  private final LEDwhite LW= new LEDwhite(le);
-  private final ShootwithLED SLED = new ShootwithLED();
-  private final DriveTrain DT = new DriveTrain();
-  private final ArcadeDrive AD = new ArcadeDrive(DT,le);
-  private final STOP stop = new STOP(DT,le);
+
   private final ExampleCommand m_autoCommand = new ExampleCommand(m_exampleSubsystem);
-  private final Joystick joystick = new Joystick(0);
-  private final JoystickButton JB = new JoystickButton(joystick, 1);
-  private final JoystickButton JB2 = new JoystickButton(joystick, 2);
+
   /** The container for the robot. Contains subsystems, OI devices, and commands. */
   public RobotContainer() {
     // Configure the button bindings
@@ -56,12 +34,7 @@ public class RobotContainer {
    * edu.wpi.first.wpilibj.Joystick} or {@link XboxController}), and then passing it to a {@link
    * edu.wpi.first.wpilibj2.command.button.JoystickButton}.
    */
-  private void configureButtonBindings() {
-    JB.whenPressed(AD);
-    JB.whenReleased(stop);
-    JB2.whenPressed(SLED.andThen(LW));
-    
-  }
+  private void configureButtonBindings() {}
 
   /**
    * Use this to pass the autonomous command to the main {@link Robot} class.
