5b9c2e54453bb162a870d28abbbc438f16bf588d
Ryutaro <ryutarounozawa@icloud.com>
2021-02-25 18:25:59 -0800

    Set contants

--- 2021/src/main/java/frc/robot/subsystems/Agitator.java
+++ 2021/src/main/java/frc/robot/subsystems/Agitator.java
@@ -8,10 +8,11 @@ import com.revrobotics.CANSparkMax;
 import com.revrobotics.CANSparkMaxLowLevel.MotorType;
 
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.Constants;
 
 public class Agitator extends SubsystemBase {
   /** Creates a new Agitator. */
-  private final CANSparkMax agitator = new CANSparkMax(2,MotorType.kBrushless);
+  private final CANSparkMax agitator = new CANSparkMax(Constants.AGITATOR_MOTOR_PORT,MotorType.kBrushless);
   public Agitator() {}
 
   public void Set_Ramp_up_time(double time){
