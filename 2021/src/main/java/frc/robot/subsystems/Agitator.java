3426a4f39ae78e09fc3871fb007a77ce921b0851
Ryutaro <ryutarounozawa@icloud.com>
2021-02-25 18:21:34 -0800

    Agitator
    you can set ramp up time and speed

---
+++ 2021/src/main/java/frc/robot/subsystems/Agitator.java
@@ -0,0 +1,31 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.subsystems;
+
+import com.revrobotics.CANSparkMax;
+import com.revrobotics.CANSparkMaxLowLevel.MotorType;
+
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
+public class Agitator extends SubsystemBase {
+  /** Creates a new Agitator. */
+  private final CANSparkMax agitator = new CANSparkMax(2,MotorType.kBrushless);
+  public Agitator() {}
+
+  public void Set_Ramp_up_time(double time){
+    agitator.setOpenLoopRampRate(time);
+  }
+
+  public void Speed(int Speed){
+    agitator.set(Speed);
+
+
+  }
+
+  @Override
+  public void periodic() {
+    // This method will be called once per scheduler run
+  }
+}
